ce33295c4ae1910dd5af60396bc3276b1bc8b34d
Luke Cheng <chenglu@users.noreply.github.com>
2020-10-13 16:45:07 +0800

    Update format.rb

--- lib/auto-correct/format.rb
+++ lib/auto-correct/format.rb
@@ -8,10 +8,7 @@ class AutoCorrect
 
   # SpecialSymbol
 # rule "#{CJK}", '[\|+$@#*]', space: true, reverse: true
-  rule "#{CJK}", '[\|+$]', space: true, reverse: true
-  
-  # Discourse
-  rule "#{CJK}", '[\|+@#*]', reverse: true
+  rule "#{CJK}", '[\|+@#]', reverse: true
 
   rule "#{CJK}", '[\[\(‘“]', space: true
   rule '[’”\]\)!%]', "#{CJK}", space: true
